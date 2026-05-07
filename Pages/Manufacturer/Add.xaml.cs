dfa9c319fee60b938b148cf65d743b4e701629c7
Linashihova <linashikhova@mail.ru.>
2026-05-07 09:42:31 +0500

    Пешин

---
+++ Pages/Manufacturer/Add.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp1;
+
+namespace PR_32.Pages.Manufacturer
+{
+    public partial class Add : Page
+    {
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Classes.Manufacturer item = new Classes.Manufacturer()
+                {
+                    Name = tbName.Text,
+                    CountryCode = 1,
+                    Phone = tbPhone.Text,
+                    Mail = tbEmail.Text
+                };
+                item.Save();
+                MessageBox.Show("Сохранено!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                MainWindow.init.OpenPage(new Main());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Pages/Manufacturer/Main.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace PR_32.Pages.Manufacturer
+{
+    public partial class Main : Page
+    {
+        public IEnumerable<Classes.Manufacturer> items;
+
+        public Main()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            items = Classes.Manufacturer.AllManufactures();
+            lvItems.ItemsSource = items;
+        }
+
+        private void TbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbSearch.Text))
+                lvItems.ItemsSource = items;
+            else
+                lvItems.ItemsSource = items.Where(x =>
+                    x.Name.ToLower().Contains(tbSearch.Text.ToLower()));
+        }
+    }
+}
