7f2e55dea1fae13358bbc8120d7484dadb84c4f1
Linashihova <linashikhova@mail.ru.>
2026-05-07 09:45:10 +0500

    Создание строниз Records Пешин

---
+++ Pages/Records/Add.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp1;
+
+namespace PR_32.Pages.Records
+{
+    public partial class Add : Page
+    {
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Classes.Record item = new Classes.Record()
+                {
+                    Name = tbName.Text,
+                    Year = int.Parse(tbYear.Text),
+                    Format = 1,
+                    Size = 300,
+                    IdManufacturer = 1,
+                    Price = float.Parse(tbPrice.Text.Replace(".", ",")),
+                    IdState = 1,
+                    Description = ""
+                };
+                item.Save();
+                MessageBox.Show("Сохранено!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                MainWindow.init.OpenPage(new Main());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Pages/Records/Main.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace PR_32.Pages.Records
+{
+    public partial class Main : Page
+    {
+        public IEnumerable<Classes.Record> items;
+
+        public Main()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            items = Classes.Record.AllRecords();
+            lvItems.ItemsSource = items;
+        }
+
+        private void TbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbSearch.Text))
+                lvItems.ItemsSource = items;
+            else
+                lvItems.ItemsSource = items.Where(x =>
+                    x.Name.ToLower().Contains(tbSearch.Text.ToLower()));
+        }
+    }
+}
